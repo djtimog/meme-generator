e81eb29c256aeb736762446c03c23dd2eaeaee47
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2025-12-15 05:54:25 +0100

    feat: add Header component, theme toggle functionality, and dropdown menu UI
    - Implemented Header component with app logo and title.
    - Created ModeToggle component for switching between light, dark, and system themes using a dropdown menu.
    - Developed ThemeProvider to manage theme state and persist user preference in local storage.
    - Added UI components for dropdown menu, including items, separators, and submenus.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Header from "./components/Header";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <>
+      <Header />
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="logo.png" alt="This is the app Logo" />
+      Meme Generator
+    </header>
+  );
+}
