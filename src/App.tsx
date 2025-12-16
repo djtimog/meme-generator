a0a02d6fc1fcace1b76cc202ccf2caf53f648d42
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2025-12-16 07:54:57 +0100

    feat: enhance meme generator functionality and UI
    - Added html2canvas and html2canvas-pro for capturing meme images.
    - Implemented meme fetching from the Imgflip API and updated state management in App component.
    - Introduced top and bottom text inputs for meme customization.
    - Enhanced ImageActionButtons component with share, copy, and download functionalities for meme images.
    - Added Skeleton and Spinner components for improved loading states.
    - Updated CSS variables for light and dark themes to improve visual consistency.
    - Refactored ImageActionButtons to handle loading states for actions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
+import { useEffect, useRef, useState } from "react";
 import Header from "./components/Header";
 import { ImageActionButtons } from "./components/Image-action-btn";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface Meme {
+  id: string;
+  name: string;
+  url: string;
+  width: number;
+  height: number;
+  box_count: number;
+}
 
 function App() {
-  const data = {
-    success: true,
-    data: {
-      memes: [
-        {
-          id: "61579",
-          name: "One Does Not Simply",
-          url: "https://i.imgflip.com/1bij.jpg",
-          width: 568,
-          height: 335,
-          box_count: 2,
-        },
-        {
-          id: "101470",
-          name: "Ancient Aliens",
-          url: "https://i.imgflip.com/26am.jpg",
-          width: 500,
-          height: 437,
-          box_count: 2,
-        },
-        // probably a lot more memes here..
-      ],
-    },
+  const [topText, setTopText] = useState<string>("");
+  const [bottomText, setBottomText] = useState<string>("");
+  const [memes, setMemes] = useState<Meme[] | null>(null);
+  const [selectedMeme, setSelectedMeme] = useState<Meme | null>(null);
+  const canvaRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => {
+        setMemes(data.data.memes);
+        setSelectedMeme(data.data.memes ? data.data.memes[0] : null);
+      });
+  }, []);
+
+  const toggleMemeImage = () => {
+    const randomIndex = Math.floor(Math.random() * (memes ? memes.length : 1));
+    if (memes) {
+      setSelectedMeme(memes[randomIndex]);
+    }
   };
+
   return (
     <>
       <Header />
@@ -41,9 +50,12 @@ function App() {
               aria-placeholder="The text that stays above the meme image"
               title="topText"
               name="topText"
+              value={topText}
+              onChange={(e) => setTopText(e.target.value)}
               className="flex-1 h-40"
             />
           </label>
+
           <label className="flex-1 flex flex-col gap-2">
             <p className="font-bold">Bottom Text</p>
             <Input
@@ -51,27 +63,59 @@ function App() {
               placeholder="E.g.,Lies We Tell Ourselves"
               aria-placeholder="The text that stays below the meme image"
               title="bottomText"
+              value={bottomText}
+              onChange={(e) => setBottomText(e.target.value)}
               name="bottomText"
               className="flex-1"
             />
           </label>
         </form>
-        <section className="flex flex-col justify-center items-center gap-7">
-          <Button>Toggle Meme Image</Button>
-          <div className="border-violet-300 border-3 max-w-sm rounded-sm overflow-hidden relative text-white">
-            <p className="absolute top-2 z-50 text-center font-extrabold text-2xl w-full">
-              This for the image
-            </p>
-            <p className="absolute bottom-2 text-center z-50 font-extrabold text-2xl w-full">
-              This for the image
-            </p>
-            <img
-              src={data.data.memes[0].url}
-              alt={data.data.memes[0].name}
-              className="object-contain h-full w-full"
-            />
-          </div>
-          <ImageActionButtons />
+
+        <section className="flex flex-col justify-center items-center gap-7 mb-10">
+          <Button onClick={toggleMemeImage}>Toggle Meme Image</Button>
+
+          {selectedMeme ? (
+            <>
+              <div
+                ref={canvaRef}
+                className="border-violet-300 border-3 max-w-sm rounded-sm overflow-hidden relative text-white text-shadow-sm text-shadow-violet-500"
+              >
+                <p className="absolute top-0 z-50 text-center font-extrabold text-3xl w-full">
+                  {topText}
+                </p>
+
+                <p className="absolute bottom-0 text-center z-50 font-extrabold text-3xl w-full">
+                  {bottomText}
+                </p>
+
+                <img
+                  src={selectedMeme.url || "https://i.imgflip.com/1bij.jpg"}
+                  alt={selectedMeme.name || "Meme Image"}
+                  crossOrigin="anonymous"
+                  className="object-contain h-full w-full"
+                />
+              </div>
+
+              {canvaRef.current && (
+                <ImageActionButtons
+                  disabled={!topText || !bottomText}
+                  canvaRef={canvaRef.current}
+                />
+              )}
+            </>
+          ) : (
+            <>
+              <div className="border-violet-300 border-3 max-w-sm rounded-sm overflow-hidden relative text-white">
+                <Skeleton className="h-60 w-96" />
+              </div>
+
+              <div className="flex gap-3">
+                {Array.from({ length: 3 }).map((_, index) => (
+                  <Skeleton key={index} className="size-7" />
+                ))}
+              </div>
+            </>
+          )}
         </section>
       </main>
     </>
--- src/components/Image-action-btn.tsx
+++ src/components/Image-action-btn.tsx
@@ -1,23 +1,114 @@
-import { Forward, Copy, ArrowDownToLine } from "lucide-react";
-
+import { Forward, Copy, ArrowDownToLine, Share } from "lucide-react";
+import html2canvas from "html2canvas-pro";
 import { Button } from "./ui/button";
+import { useState } from "react";
+import { Spinner } from "./ui/spinner";
 
-export function ImageActionButtons() {
+export function ImageActionButtons({
+  disabled,
+  canvaRef,
+}: {
+  disabled?: boolean;
+  canvaRef: HTMLDivElement;
+}) {
+  const [isLoading, setIsLoading] = useState<boolean[]>([false, false, false]);
+
+  const getCanvaImageUrl = async () => {
+    return await html2canvas(canvaRef, { useCORS: true }).then((canva) => {
+      const dataURL = canva.toDataURL("image/png");
+      return dataURL;
+    });
+  };
+
+  async function urlToFile(
+    url: string,
+    filename: string,
+    mimeType?: string
+  ): Promise<File> {
+    const res = await fetch(url);
+    const blob = await res.blob();
+    return new File([blob], filename, { type: mimeType || blob.type });
+  }
+
+  const IconStatus = [
+    {
+      icon: Forward,
+      label: "forward",
+      action: async () => {
+        const canvaUrl = await getCanvaImageUrl();
+        const file = await urlToFile(canvaUrl, "capture.png", "image/png");
+
+        if (navigator.canShare && navigator.canShare({ files: [file] })) {
+          try {
+            await navigator.share({
+              files: [file],
+              title: "Shared Canvas",
+              text: "Check out this image!",
+            });
+          } catch (err) {
+            console.error("Share failed", err);
+          }
+        } else {
+          alert("Web Share API not supported in this browser.");
+        }
+      },
+    },
+    {
+      icon: Copy,
+      label: "copy-file",
+      action: async () => {
+        const canvaUrl = await getCanvaImageUrl();
+        const file = await urlToFile(canvaUrl, "capture.png", "image/png");
+
+        try {
+          await navigator.clipboard.write([
+            new ClipboardItem({
+              [file.type]: file,
+            }),
+          ]);
+        } catch (err) {
+          console.error("Clipboard copy failed", err);
+          alert("Clipboard file copy not supported in this browser.");
+        }
+      },
+    },
+    {
+      icon: ArrowDownToLine,
+      label: "download",
+      action: async () => {
+        const canvaUrl = await getCanvaImageUrl();
+        const link = document.createElement("a");
+        link.href = canvaUrl;
+        link.download = "capture.png";
+        link.click();
+      },
+    },
+  ];
+
+  const handleClick = async (action: () => Promise<void>, index: number) => {
+    setIsLoading((loadingsPrev) =>
+      loadingsPrev.map((loading, i) => (i === index ? true : loading))
+    );
+    await action();
+    setIsLoading((loadingsPrev) =>
+      loadingsPrev.map((loading, i) => (i === index ? false : loading))
+    );
+  };
+
   return (
     <div className="flex gap-3">
-      <Button value="Forward" size={"icon"} aria-label="Toggle Forward">
-        <Forward className="h-4 w-4" />
-      </Button>
-      <Button value="Copy" size={"icon"} aria-label="Toggle Copy">
-        <Copy className="h-4 w-4" />
-      </Button>
-      <Button
-        value="strikethrough"
-        size={"icon"}
-        aria-label="Toggle strikethrough"
-      >
-        <ArrowDownToLine className="h-4 w-4" />
-      </Button>
+      {IconStatus.map((icon, index) => (
+        <Button
+          key={icon.label}
+          disabled={disabled || isLoading[index]}
+          value={icon.label}
+          size={"icon"}
+          onClick={() => handleClick(icon.action, index)}
+          aria-label={`Toggle ${icon.label}`}
+        >
+          {isLoading[index] ? <Spinner /> : <icon.icon className="h-4 w-4" />}
+        </Button>
+      ))}
     </div>
   );
 }
